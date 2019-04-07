0916a39546eeef4a950c6ec7c2418075912cb7e4
marcio.costa <Dev4fun>
2019-04-07 17:43:05 -0300

    added mocks on the tests

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,11 +1,8 @@
 class Enviador:
-    def __init__(self):
-        self.qtde_email_enviados = 0
 
     def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
             raise Emailinvalido(f'Email de remetente inválido: {remetente}')
-        self.qtde_email_enviados += 1
         return remetente
 
 
--- libpythonpro/testes/test_spam/test_envio_para_base_de_usuario.py
+++ libpythonpro/testes/test_spam/test_envio_para_base_de_usuario.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro.spam.enviador_de_email import Enviador
@@ -17,13 +19,34 @@ from libpythonpro.spam.modelos import Usuario
         ]
     ]
 )
+
+
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'renzo@pythonpro.com.br',
         'Curso Python Pro',
         'confira os módulos fantásticos'
     )
-    assert len(usuarios) == enviador.qtde_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Renzo', email='renzo@pythonpro.com')
+    sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'luciano@pythonpro.com',
+        'Curso Python Pro',
+        'confira os módulos fantásticos'
+    )
+    enviador.enviar.assert_called_once_with == (
+        'luciano@pythonpro.com',
+        'renzo@pythonpro.com',
+        'Curso Python Pro',
+        'confira os módulos fantásticos'
+    )
