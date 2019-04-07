b95a0a264132bb3afca10e11bb96124184092cbe
marcio.costa <Dev4fun>
2019-04-07 01:17:08 -0300

    add SPAM

--- libpythonpro/testes/test_spam/test_usuarios.py
+++ libpythonpro/testes/test_spam/test_usuarios.py
@@ -1,24 +1,17 @@
-from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome = 'Marcio')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
-def test_listar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+
+def test_listar_usuario(sessao):
     usuarios = [Usuario(nome = 'Marcio'), Usuario(nome = 'Renzo')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
+
+
+
